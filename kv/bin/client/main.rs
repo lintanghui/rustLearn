bc3fa617b97b0d894031936a429ebaa35becaaaa
lintanghui <lintanghui@bilibili.com>
2018-12-30 23:49:06 +0800

    tcp net

--- kv/bin/client/main.rs
+++ kv/bin/client/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    kv::client("127.0.0.1:8000")
+    kv::client();
 }
--- kv/src/lib.rs
+++ kv/src/lib.rs
@@ -1,23 +1,46 @@
+use std::thread;
 use tokio::io;
 use tokio::io::copy;
 use tokio::net::*;
 use tokio::prelude::*;
-pub fn client(addr: &str) {
-    let host = addr.parse().unwrap();
-    let client = TcpStream::connect(&host)
-        .map_err(|err| eprintln!("connet err {:}", err))
-        .and_then(|sock| {
-            let mut input = String::new();
-            match std::io::stdin().read_line(&mut input) {
-                Ok(n) => {
-                    io::write_all(sock, input);
-        
+pub fn client() {
+    let mut stream = std::net::TcpStream::connect("127.0.0.1:8000").unwrap();
+    let mut input = stream.try_clone().unwrap();
+    let handler = thread::spawn(move || {
+        let mut buffer = [0u8; 1024];
+        loop {
+            match input.read(&mut buffer) {
+                Ok(_n) => {
+                    std::io::stdout().write(&buffer).unwrap();
+                    std::io::stdout().flush().unwrap();
                 }
-                Err(error) => println!("error: {}", error),
+                Err(error) => eprintln!("err read"),
             }
-            Ok(())
-        });
-    tokio::run(client);
+        }
+    });
+    let output = &mut stream;
+    let mut data = String::new();
+    loop {
+        std::io::stdin().read_line(&mut data);
+        output.write(data.as_bytes()).unwrap();
+        output.flush().unwrap();
+    }
+    // let client = TcpStream::connect(&host)
+    //     .map_err(|err| eprintln!("connet err {:}", err))
+    //     .and_then(|sock| {
+
+    //         let mut input = String::new();
+    //         match std::io::stdin().read_line(&mut input) {
+    //             Ok(n) => {
+    //                 io::write_all(sock, input);
+
+    //             }
+    //             Err(error) => println!("error: {}", error),
+    //         }
+
+    //         Ok(())
+    //     });
+    // tokio::run(client);
 }
 
 pub fn server(addr: &str) {
@@ -32,7 +55,7 @@ pub fn server(addr: &str) {
             let handle = byte_copied
                 .map(|amt| println!("wrote {:}", amt.0))
                 .map_err(|err| eprintln!("io err {:}", err));
-            tokio::spawn(handle)    
+            tokio::spawn(handle)
         });
     tokio::run(server);
 }
